90b7f290e4e0d684ae30cad69627da1da3452b2d
Bryant <109110061+bryantolmos@users.noreply.github.com>
2024-08-01 12:06:49 -0700

    Updated read Yaml

--- readYaml.py
+++ readYaml.py
@@ -1,19 +1,34 @@
 import yaml
 import yaml.scanner
 
-# Path to yaml file
-yaml_file_name = "sample.yaml"
+def read_yaml(yaml_file_name: str, print_yaml : bool) -> dict:
+    """
+    Reads a YAML file and returns its contents as a dictionary.
 
-try:
-    with open(yaml_file_name, 'r') as file:
-        yaml_file = yaml.safe_load(file)
+    Parameters:
+    yaml_file_name (str): The path to the YAML file.
 
-    print("Yaml File Contents:")
-    for keys in yaml_file:
-        print(f"{keys} : {yaml_file[keys]}")
-except yaml.scanner.ScannerError:
-    print("\n## Syntax Error In Yaml File  ##\n## Scanner Error In Yaml File ##\n")
-except FileNotFoundError:
-    print("\n## Yaml File Not Found    ##\n## Or File Does Not Exits ##\n")
-except Exception as e:
-    print(f"\n## Error: {e} ##\n")
+    Returns:
+    dict: The contents of the YAML file as a dictionary.
+
+    Raises:
+    FileNotFoundError: If the file does not exist.
+    yaml.YAMLError: If there is an error in the YAML syntax.
+    Exception: For any other exceptions that occur.
+    """
+    try:
+        with open(yaml_file_name, 'r') as file:
+            yaml_file = yaml.safe_load(file)
+            if print_yaml == True:
+                for keys in yaml_file:
+                    print(f"{keys} : {yaml_file[keys]}")
+            return yaml_file
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"\n\nYAML file not found: {yaml_file_name}\n")
+
+    except yaml.YAMLError as e:
+        raise yaml.YAMLError(f"\n\nSyntax error in YAML file: {yaml_file_name} - {e}\n")
+
+    except Exception as e:
+        raise Exception(f"\n\nAn error occurred while reading the YAML file: {e}\n")
