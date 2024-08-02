f5a921cccecedda4b3af89cb3d998faf80eb49c5
Bryant <109110061+bryantolmos@users.noreply.github.com>
2024-08-02 11:13:58 -0700

    read yaml update

--- readYaml.py
+++ readYaml.py
@@ -2,20 +2,6 @@ import yaml
 import yaml.scanner
 
 def read_yaml(yaml_file_name: str, print_yaml : bool) -> dict:
-    """
-    Reads a YAML file and returns its contents as a dictionary.
-
-    Parameters:
-    yaml_file_name (str): The path to the YAML file.
-
-    Returns:
-    dict: The contents of the YAML file as a dictionary.
-
-    Raises:
-    FileNotFoundError: If the file does not exist.
-    yaml.YAMLError: If there is an error in the YAML syntax.
-    Exception: For any other exceptions that occur.
-    """
     try:
         with open(yaml_file_name, 'r') as file:
             yaml_file = yaml.safe_load(file)
