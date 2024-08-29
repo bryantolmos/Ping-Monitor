fe9cbd55f756f3b82128d41964259f4c454a1423
Bryant <109110061+bryantolmos@users.noreply.github.com>
2024-08-29 10:48:15 -0700

    updated ping monitor

--- pingmonitor.py
+++ pingmonitor.py
@@ -1,9 +1,23 @@
 import yaml
 import pingparsing
 import os
+import time
 import concurrent.futures
+from prometheus_client import start_http_server, Gauge
+
+# initialize metrics with server label - global
+packet_transmit = Gauge("packet_transmit", "packets transmitted",["server"])
+packet_receive = Gauge("packet_receive", "packets received",["server"])
+packet_loss_rate = Gauge("packet_loss_rate", "packet loss rate",["server"])
+rtt_min = Gauge("rtt_min", "round trip time min in milliseconds",["server"])
+rtt_avg = Gauge("rtt_avg", "round trip time avg in milliseconds",["server"])
+rtt_max = Gauge("rtt_max", "round trip time max in milliseconds",["server"])
+rtt_mdev = Gauge("rtt_mdev", "round trip time population standard deviation",["server"])
+
+ping_success_rate = Gauge("ping_success_rate", "Ping success rate", ["server"])
 
 def parse_yaml(yaml_file_name: str) -> dict:
+    print(f"Looking for YAML file at: {os.path.abspath(yaml_file_name)}")
     try:
         with open(yaml_file_name, 'r') as file:
             yaml_file_contents = yaml.safe_load(file)
@@ -15,52 +29,65 @@ def parse_yaml(yaml_file_name: str) -> dict:
     except Exception as e:
         raise Exception(f"\n\nAn error occurred while reading the YAML file: {e}\n")
 
-def ping_parse(ping_destination : str, ping_count : int) -> str:
+def ping_parse(ping_destination : str, frequency: int) -> None:
     ping_parser = pingparsing.PingParsing()
     transmitter = pingparsing.PingTransmitter()
     transmitter.destination = ping_destination
-    transmitter.count = ping_count
 
-    ping_results = ping_parser.parse(transmitter.ping())
-    icmp_replies_list = ping_results.icmp_replies
+    successful_pings = 0
+    total_pings = 0
 
-    ping_data = {
-        "ping_results": {
-            "packet_transmit": ping_results.packet_transmit,
-            "packet_receive": ping_results.packet_receive,
-            "packet_loss_rate": ping_results.packet_loss_rate,
-            "rtt_min": ping_results.rtt_min,
-            "rtt_avg": ping_results.rtt_avg,
-            "rtt_max": ping_results.rtt_max,
-            "rtt_mdev": ping_results.rtt_mdev,
-        },
-        "icmp_replies": icmp_replies_list
-    }
 
-    base_file_name = f"ping_results_{ping_destination.replace('.', '_')}"
-    output_file = f"{base_file_name}.yaml"
+    while True:
+        ping_results = ping_parser.parse(transmitter.ping())
+        icmp_replies_list = ping_results.icmp_replies
 
-    counter = 1
-    while os.path.exists(output_file):
-        output_file = f"{base_file_name}_{counter}.yaml"
-        counter += 1
+        total_pings += 1
+        if ping_results.packet_receive > 0:
+            successful_pings += 1
 
-    with open(output_file, 'w') as file:
-        yaml.dump(ping_data, file)
+        ping_data = {
+            "ping_results": {
+                "packet_transmit": ping_results.packet_transmit,
+                "packet_receive": ping_results.packet_receive,
+                "packet_loss_rate": ping_results.packet_loss_rate,
+                "rtt_min": ping_results.rtt_min,
+                "rtt_avg": ping_results.rtt_avg,
+                "rtt_max": ping_results.rtt_max,
+                "rtt_mdev": ping_results.rtt_mdev,
+            },  
+            "icmp_replies": icmp_replies_list
+        }
 
-    return output_file
+        # set the metric values
+        packet_transmit.labels(server=ping_destination).set(ping_data["ping_results"]["packet_transmit"])
+        packet_receive.labels(server=ping_destination).set(ping_data["ping_results"]["packet_receive"])
+        packet_loss_rate.labels(server=ping_destination).set(ping_data["ping_results"]["packet_loss_rate"])
+        rtt_min.labels(server=ping_destination).set(ping_data["ping_results"]["rtt_min"])
+        rtt_max.labels(server=ping_destination).set(ping_data["ping_results"]["rtt_max"])
+        rtt_avg.labels(server=ping_destination).set(ping_data["ping_results"]["rtt_avg"])
+        rtt_mdev.labels(server=ping_destination).set(ping_data["ping_results"]["rtt_mdev"])
+
+        success_rate = (successful_pings/total_pings)*100
+        ping_success_rate.labels(server=ping_destination).set(success_rate)
+
+        time.sleep(frequency)
+
 
 def caller(config_yaml : str) -> None:
-    config_contents = parse_yaml(config_yaml)
-    
-    def worker(ip, count):
-        ping_parse(ip, count)
+    script_dir = os.path.dirname(__file__)  # Get the directory where the script is located
+    config_yaml_path = os.path.join(script_dir, config_yaml)  # Build the full path
+    config_contents = parse_yaml(config_yaml_path)
+
+    def worker(ip, frequency):
+        ping_parse(ip, frequency)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
         for i in range(1, len(config_contents)+1):
             ip = config_contents[f'address{i}']['ip']
-            count = config_contents[f'address{i}']['count']
-            futures.append(executor.submit(worker, ip, count))
+            frequency = config_contents[f'address{i}']['frequency']
+            executor.submit(worker, ip, frequency)
 
-        concurrent.futures.wait(futures)
+def start(config_yaml : str) -> None:
+    start_http_server(8989)
+    caller(config_yaml)
